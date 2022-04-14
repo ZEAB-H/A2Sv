74ccbf8c5d0bb323f9d690249734ce00897e6769
abeldejene30 <96728745+ZEAB-H@users.noreply.github.com>
2022-04-14 20:45:08 +0300

    Creating Fizz Buzz solution 1

---
+++ Fuzz_buzz.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result(n, "");
+        for(int i = 1; i < n+1; ++i){
+       
+          //checking divisiblity and 
+            if(i % 15 == 0) result[i-1] = "FizzBuzz";
+          
+            else if (i % 5 == 0) result[i-1] = "Buzz";
+           
+            else if (i % 3 == 0) result[i-1] = "Fizz";
+            else result[i-1] = to_string(i);
+        }
+        return result;
+    }
+};
