10d0692411066c90bafc0db297e38473be1195a9
abeldejene30 <96728745+ZEAB-H@users.noreply.github.com>
2022-05-09 18:12:46 +0300

    changing the selection sort answer

--- sorting the sentence/sorting_the_sentence.py
+++ sorting the sentence/sorting_the_sentence.py
@@ -1,11 +1,34 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        temp= s.split()
-        word={}
-        ans=''
-        for i in temp:
-            word[i[-1]]= i[:-1]
-        for i in sorted(word):
-            ans=ans+''.join(word[i])+' '
-        ans=ans[:-1]
-        return ans
+        final=[""]*10
+        
+        current=0
+        
+        temp= ""
+        
+        while current<len(s):
+            if s[current]==" ":
+                current +=1
+            try:
+                index=int(s[current])
+                final[index]=temp
+                current +=1
+                temp= ""
+            except:
+                temp += s[current]
+                current +=1
+               
+        string =""
+        
+        for word in final:
+            if len(word)== 0:
+                continue
+            if len(string)==0:
+                string = word
+            else :
+                string +=" " + word 
+        
+        
+        return string
+            
+              
