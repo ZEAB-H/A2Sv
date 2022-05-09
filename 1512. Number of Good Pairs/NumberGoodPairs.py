de4d19c2011c623c88e2dfaf38056146a134aca2
abel-dejene <abelhailemichael30@gmail.com>
2022-05-09 18:08:51 +0300

    adding the solution

---
+++ 1512. Number of Good Pairs/NumberGoodPairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        good_pairs= 0
+        d={}
+        for num in nums:
+            if num not in d :
+                d[num]=1
+            else:
+                d[num] +=1
+                good_pairs += d[num]-1
+            
+        return good_pairs
+  
+                
+   
